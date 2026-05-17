657e8d46f9274543c9ac8dda0a250b816e08b543
Benjamin <fireblazegaming21@gmail.com>
2026-05-18 00:01:43 +0200

    Add nal1 and nal2

--- Dn/dn10/naloga1/naloga1.c
+++ Dn/dn10/naloga1/naloga1.c
@@ -6,44 +6,11 @@
 
 #include "naloga1.h"
 
-typedef struct _vo_vec {
-    int size;
-    int last;
-    VO** vec;
-} vo_vec;
-
-VO* get(const vo_vec* vec,int i)
-{
-    if (vec == NULL) return NULL;
-    return vec->vec[i];
-}
-
-void resizeVector(vo_vec* vec)
-{
-    if (vec == NULL) return;
-    VO** arr = (VO**)malloc(sizeof(VO*) * 2*(vec->size));
-    for (int i = 0; i < vec->size; i++)
-        arr[i] = get(vec,i);
-    vec->size = 2*(vec->size);
-
-    vec->vec = arr;
-}
-
-bool add(vo_vec* vec, VO* vo)
-{
-    if (vec == NULL) return false;
-    if (vec->last >= vec->size)
-        resizeVector(vec);
-    vec->vec[vec->last] = vo;
-    vec->last++;
-    return true;
-}
-
-VO* createVO(Student* s, PO* pred)
+VO* createVO(Student* s, PO pred)
 {
     VO* vo = (VO*)malloc(sizeof(VO));
     vo->vpisna = s->vpisna;
-    vo->ocena = pred->ocena;
+    vo->ocena = pred.ocena;
     return vo;
 }
 
@@ -58,6 +25,62 @@ bool containsSubject(Student* s, char* subject, int* sub_idx)
     return false;
 }
 
+void freeArr(VO** arr, int len)
+{
+    for (int i = 0; i < len; i++)
+        free(arr[i]);
+    free(arr);
+}
+
+void orderArray(VO** arr, int const len)
+{
+    if (arr == NULL) exit(1);
+    for (int i = 0; i < len; i++)
+    {
+        int mxIdx = i;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[j]->ocena > arr[mxIdx]->ocena)
+                mxIdx = j;
+        }
+
+        if (mxIdx == i) continue;
+        VO* tmp = arr[i];
+        arr[i] = arr[mxIdx];
+        arr[mxIdx] = tmp;
+
+    }
+
+    for (int i = 0; i < len - 1; i++)
+    {
+        int sub_len = 0;
+        for (int j = i; j < len - 1 && arr[j]->ocena == arr[j+1]->ocena;j++) sub_len++;
+
+        // printf("sub_len: %d\n", sub_len);
+        if (sub_len == 0) continue;
+        sub_len++;
+        // printf("alt_sub_len: %d\n", sub_len);
+
+        for (int j = i; j < sub_len; j++)
+        {
+            int mxIdx = i;
+            for (int k = j + 1; k < sub_len; k++)
+            {
+                if (arr[k]->vpisna > arr[mxIdx]->vpisna)
+                    mxIdx = k;
+            }
+
+            if (mxIdx == j) continue;
+            VO* tmp = arr[j];
+            arr[j] = arr[mxIdx];
+            arr[mxIdx] = tmp;
+        }
+        i += sub_len;
+
+    }
+
+}
+
 VO** opravili(Student** studentje, int stStudentov, char* predmet, int* stVO) {
     // popravite / dopolnite ...
     int len = 0;
@@ -68,13 +91,26 @@ VO** opravili(Student** studentje, int stStudentov, char* predmet, int* stVO) {
             len++;
     }
 
-    return NULL;
+    *stVO = len;
+
+    VO** arr = (VO**)malloc(sizeof(VO*) * len);
+    int c = 0;
+    for (int i = 0 ; i < stStudentov; i++)
+    {
+        int sub_idx = 0;
+        if (containsSubject(studentje[i], predmet, &sub_idx) && studentje[i]->po[sub_idx].ocena >= 6)
+            arr[c++] = createVO(studentje[i], studentje[i]->po[sub_idx]);
+
+    }
+    orderArray(arr, len);
+    return arr;
 }
 
 #ifndef test
 
 int main() {
     // koda za ro"cno testiranje (po "zelji)
+
     return 0;
 }
 
--- Dn/dn10/naloga2/naloga2.c
+++ Dn/dn10/naloga2/naloga2.c
@@ -6,6 +6,11 @@
 
 #include "naloga2.h"
 
+void reduce(Ulomek* u)
+{
+    double rat = u->st / u->im;
+}
+
 Tocka projekcija(Tocka t, Premica p) {
     // popravite / dopolnite ...
     return (Tocka) { (Ulomek) {0, 1}, (Ulomek) {0, 1} };
